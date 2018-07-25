7d128725a10c3bc0e65a8ab1d8631675a971ffef
AlloVince <allo.vince@gmail.com>
2018-07-25 17:53:38 +0800

    fix: add release.js

--- release.js
+++ release.js
@@ -1,10 +1,13 @@
-// ==UserScript==
+const fs = require('fs');
+const [, , version] = process.argv;
+fs.writeFileSync('index.js',
+  `// ==UserScript==
 // @name         银杏
 // @namespace    yinxing
-// @version      1.0.0
+// @version      ${version}
 // @description  Quick copy & send magnet links
 // @author       AlloVince
-// @require      https://cdn.jsdelivr.net/npm/yinxing.monkey/lib/index.js
+// @require      https://cdn.jsdelivr.net/npm/yinxing.monkey@${version}/lib/index.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
 // @grant        GM_notification
@@ -26,4 +29,7 @@
 // @grant        unsafeWindow
 // @run-at       document-start
 // @include      http*
-// ==/UserScript==
+// ==/UserScript==`);
+
+
+
